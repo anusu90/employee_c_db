cf90e8be3091277cb0baf8f089e00838d92a5d8e
anusu90 <anusu90@gmail.com>
2026-01-02 20:03:17 +0530

    setting up getopt with basic options

--- src/main.c
+++ src/main.c
@@ -1,9 +1,36 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 
-    printf("Hello world\n");
+    int flag;
+    bool newFile = false;
+    char *filepath = NULL;
+
+    while ((flag = getopt(argc, argv, "nf:")) != -1)
+    {
+        switch (flag)
+        {
+        case 'n':
+            newFile = true;
+            break;
+        case 'f':
+            filepath = optarg;
+            break;
+        case '?':
+            printf("Unknown option: %c\n", optopt);
+            break;
+        default:
+            printf("Hello world\n");
+            return -1;
+        }
+    }
+
+    printf("New file: %d\n", newFile);
+    printf("Filepath: %s\n", filepath);
 
     return 0;
 }
