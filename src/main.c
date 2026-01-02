e9056b762319576a48907a8231bc198e9e5db480
anusu90 <anusu90@gmail.com>
2026-01-02 20:40:41 +0530

    added check for filepath

--- src/main.c
+++ src/main.c
@@ -3,12 +3,21 @@
 #include <getopt.h>
 #include <stdlib.h>
 
+void print_usage(char *argv[])
+{
+
+    printf("Usage: %s -n -f <file>\n", argv[0]);
+    printf("Options:\n");
+    printf("\t-n      Create a new file\n");
+    printf("\t-f <file>  Specify the file to use\n");
+}
+
 int main(int argc, char *argv[])
 {
 
     int flag;
     bool newFile = false;
-    char *filepath = NULL;
+    char *filePath = NULL;
 
     while ((flag = getopt(argc, argv, "nf:")) != -1)
     {
@@ -18,7 +27,7 @@ int main(int argc, char *argv[])
             newFile = true;
             break;
         case 'f':
-            filepath = optarg;
+            filePath = optarg;
             break;
         case '?':
             printf("Unknown option: %c\n", optopt);
@@ -29,8 +38,15 @@ int main(int argc, char *argv[])
         }
     }
 
+    if (filePath == NULL)
+    {
+        printf("No file path provided\n");
+        print_usage(argv);
+        return 1;
+    }
+
     printf("New file: %d\n", newFile);
-    printf("Filepath: %s\n", filepath);
+    printf("Filepath: %s\n", filePath);
 
     return 0;
 }
